bbe247805215b92417e7d75592b4541cfcf57e41
darylekarldevera <daryle.devera@cubework.com.ph>
2024-09-08 17:42:50 +0800

    Add DummyComponent for placeholder routes' component and restrictedRoute constants

---
+++ src/constant/restrictedRoute.ts
@@ -0,0 +1,15 @@
+const UNAUTHENTICATED_ROUTES = [
+  '/login',
+  '/forgot-password',
+  '/reset-password',
+];
+
+const AUTHENTICATED_ROUTES = [
+  '/home',
+  '/profile',
+];
+
+export {
+  UNAUTHENTICATED_ROUTES,
+  AUTHENTICATED_ROUTES,
+};

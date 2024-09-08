68cc464ea54cbec91df5cfa1d8ac77e8f7dd71c8
darylekarldevera <daryle.devera@cubework.com.ph>
2024-09-09 07:43:42 +0800

    Refactor SidebarNavigation component and add navigation items constants

--- src/components/SidebarNavigation.tsx
+++ src/components/SidebarNavigation.tsx
@@ -1,9 +1,24 @@
+import NAVIGATION_ITEMS from "@/constants/navigationItem";
+
 function SidebarNavigation() {
   return (
-    <div>
-      this is sidebar
+    <div className="border-r-2 border-grey-500 flex flex-col w-[18%]">
+      <div className="h-[25%] flex bg-cover bg-center object-contain bg-no-repeat bg-[url('https://placehold.co/600x400')]">
+        <div className="bg-white/[.7] mt-auto mb-2 relative z-10 w-full h-[50%]">
+          <p>Company Name</p>
+          <p>Branch name | branch name</p>
+        </div>
+      </div>
+      <div className="pb-3 bg-white" />
+      <div className="flex-grow-3 border-t-2 border-grey-500">
+        {NAVIGATION_ITEMS.map((item, index) => (
+          <div key={index} className="p-3 text-center cursor-pointer">
+            <p>{item}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default SidebarNavigation
---
+++ src/constants/navigationItem.ts
@@ -0,0 +1,9 @@
+const NAVIGATION_ITEMS = [
+  { name: 'Home', path: '/home' },
+  { name: 'Lease Profile', path: '/lease-profile/contacts' },
+  { name: 'Payments', path: '/payments/make-payments/' },
+  { name: 'Documents', path: '/lease-documents/invoice' },
+  { name: 'Services', path: '/services/details' },
+];
+
+export default NAVIGATION_ITEMS;

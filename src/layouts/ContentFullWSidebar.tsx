a39acdb92b57110a6c909bd1683156cc905d2294
darylekarldevera <daryle.devera@cubework.com.ph>
2024-09-10 13:25:20 +0800

    Refactor ContentFullWidthWSidebar component to improve responsiveness

--- src/layouts/ContentFullWSidebar.tsx
+++ src/layouts/ContentFullWSidebar.tsx
@@ -9,8 +9,8 @@ interface IContentFullWidthWSidebarProps {
 export default function ContentFullWidthWSidebar({ sidebar, content }: IContentFullWidthWSidebarProps) {
   return (
     <div className="layout--full-width-w-sidebar flex flex-col md:flex-row h-[100%] overflow-hidden">
-      <div className="w-full md:w-60">{sidebar ?? <SidebarNavigation />}</div>
-      <div className="flex-grow-0 md:flex-grow ">{content ?? 'Content Full Width w/ Sidebar'}</div>
+      <div className="w-[15vw]">{sidebar ?? <SidebarNavigation />}</div>
+      <div className="flex-grow-0 md:flex-grow h-[100%]">{content}</div>
     </div>
   );
 }

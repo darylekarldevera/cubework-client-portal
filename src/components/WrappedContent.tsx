71bbc41b6f8526558eba35e750244b5906ce4bf0
darylekarldevera <daryle.devera@cubework.com.ph>
2024-09-11 04:05:39 +0800

    Refactor WrappedContent component to add optional className prop

--- src/components/WrappedContent.tsx
+++ src/components/WrappedContent.tsx
@@ -1,7 +1,14 @@
-function WrappedContent({ children }: { children: React.ReactNode }) {
+import React from 'react';
+
+interface WrappedContentProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+function WrappedContent({ children, className = '' }: WrappedContentProps) {
   return (
-    <div className="h-full flex justify-center w-full sm:max-w-[100vw] md:max-w-[80vw] lg:max-w-[100vw] m-auto">
-      <div className="flex flex-col justify-around h-[95%] flex-grow xxs:max-w-[95%] md:max-w-[80%]">
+    <div className={`h-full flex justify-center w-full sm:max-w-[100vw] md:max-w-[80vw] lg:max-w-[100vw] m-auto ${className}`}>
+      <div className="flex flex-col justify-around h-[100%] flex-grow xxs:max-w-[95%] md:max-w-[80%]">
         {children}
       </div>
     </div>

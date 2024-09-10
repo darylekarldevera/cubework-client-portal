38c08b6eeced97205dbd94bdd6ab8e7254c116f6
darylekarldevera <daryle.devera@cubework.com.ph>
2024-09-10 13:26:53 +0800

    Refactor HomePaymentDue component to improve responsiveness and update styles following figma design

--- src/components/HomePaymentDue.tsx
+++ src/components/HomePaymentDue.tsx
@@ -1,10 +1,23 @@
+import manageAutoPayIcon from '@/assets/icons/manage-auto-pay-icon.svg';
+
 function HomePaymentDue() {
   return (
-    <div className="bg-[#59BA56] text-gray-50 w-full h-[20%] px-10 flex flex-col justify-center items-start">
-      <p>Due</p>
-      <div className="w-[100%] text-lg font-bold flex justify-between flex-grow-1 border-solid">
-        <p>$ 8000.88</p>
-        <button className="px-7 py-1.5 text-[#59BA56] font-bold bg-white rounded text-sm">Pay Now</button>
+    <div
+      className="xxs:h-[20%] md:h-[20%] bg-gradient-to-r from-[#4EB951] to-[#FFFFFF] m-auto flex flex-col w-full justify-center items-start rounded-t-[2vh] rounded-b-[4vh]"
+      style={{ boxShadow: '0px 0px 32px 0px rgba(0, 0, 0, 0.25)' }}
+    >
+      <div className="flex flex-grow text-gray-50 w-full px-7 py-2 flex-row items-center">
+        <div className="w-[100%] font-bold flex flex-col">
+          <p>Balance</p>
+          <p className="text-2xl">$ 8000.88</p>
+        </div>
+        <button className="w-[142px] h-[44px] bg-[#59BA56] font-bold text-white rounded-3xl text-sm`">Pay Now</button>
+      </div>
+      <div className="bg-white flex text-xs flex-grow w-full items-center py-2 px-7">
+        <div className="flex cursor-pointer">
+          <img alt="manage_auto_pay" src={manageAutoPayIcon} width="15px" className="mr-1" />
+          <p>Manage Auto Pay</p>
+        </div>
       </div>
     </div>
   );

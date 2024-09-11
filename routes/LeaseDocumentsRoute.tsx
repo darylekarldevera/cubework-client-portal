eb8da2a4ab7605677ab352d4e10b6e9f0db435bf
darylekarldevera <daryle.devera@cubework.com.ph>
2024-09-12 07:24:23 +0800

    Add Documents and StatementInvoiceDocument components to LeaseDocumentsRoute

--- routes/LeaseDocumentsRoute.tsx
+++ routes/LeaseDocumentsRoute.tsx
@@ -1,13 +1,15 @@
 import { Navigate, Route } from 'react-router-dom'
 import DummyComponent from './../src/components/DummyComponent';
 import WrappedRoute from './WrappedRoute';
+import Documents from '@/components/document-page/Documents';
+import StatementInvoiceDocument from './../src/components/document-page/StatementInvoiceDocument';
 
 function LeaseDocumentsRoutes() {
   return (
     <WrappedRoute>
-      <Route element={<DummyComponent text="Lease Documents" />}>
+      <Route element={<Documents  /> }>
         <Route index element={<Navigate to="invoice" replace />} />
-        <Route path="invoice" element={<DummyComponent text="Statements/Invoice" />} />
+        <Route path="invoice" element={<StatementInvoiceDocument />} />
         <Route path="overview" element={<DummyComponent text="Lease Documents" />} />
       </Route>
     </WrappedRoute>

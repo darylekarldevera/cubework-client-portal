ad7997336381895658a0b52c562aab990f16ad4f
Alfred Capacete <alfred.capacete@item.com>
2024-09-11 12:43:13 +0800

    CUBE-43 Add lease mock data and queries

--- routes/LeaseProfileRoute.tsx
+++ routes/LeaseProfileRoute.tsx
@@ -1,16 +1,18 @@
 import { Navigate, Route } from 'react-router-dom';
 import DummyComponent from '@/components/DummyComponent';
 import WrappedRoute from './WrappedRoute';
-import Lease from '@/components/Lease';
+import LeaseContacts from '@/components/LeaseContacts';
+import LeaseMySpace from '@/components/LeaseMySpace';
+import LeaseChargeSchedule from '@/components/LeaseChargeSchedule';
 
 function LeaseProfileRoute() {
   return (
     <WrappedRoute>
       <Route element={<DummyComponent text="Lease Profile" />}>
         <Route index element={<Navigate to="contacts" replace />} />
-        <Route index path="contacts" element={<Lease />} />
-        <Route path="my-space" element={<DummyComponent text="My Space(s)" />} />
-        <Route path="charge-schedule" element={<DummyComponent text="Charge Schedule" />} />
+        <Route index path="contacts" element={<LeaseContacts />} />
+        <Route path="my-space" element={<LeaseMySpace />} />
+        <Route path="charge-schedule" element={<LeaseChargeSchedule />} />
       </Route>
     </WrappedRoute>
   );
--- src/queries/LeaseQuery.ts
+++ src/queries/LeaseQuery.ts
@@ -15,6 +15,30 @@ interface ILeaseItems {
   data: ILeaseItem[],
 }
 
+interface ILeaseMySpace {
+  id: number,
+  start_date: string,
+  end_date: string,
+  move_in_date: string,
+}
+
+interface ILeaseMySpaceItems {
+  data: ILeaseMySpace[],
+}
+
+interface ILeaseChargeSchedule {
+  id: number,
+  description: string,
+  period: string,
+  billing_frequency: string,
+  units: string,
+
+}
+
+interface ILeaseChargeScheduleItems {
+  data: ILeaseChargeSchedule[];
+}
+
 function leaseQuery(endpoint: string, page: number = 1, perPage: number = 5) {
   return useQuery<ILeaseItems>({
     queryKey: [endpoint, page, perPage],
@@ -31,6 +55,37 @@ function leaseQuery(endpoint: string, page: number = 1, perPage: number = 5) {
   });
 }
 
-export { leaseQuery };
-export type { ILeaseItem, ILeaseItems };
+function leaseMySpaceQuery(endpoint: string, page: number = 1, perPage: number = 5) {
+  return useQuery<ILeaseItems>({
+    queryKey: [endpoint, page, perPage],
+    queryFn: async () => {
+      const resp = await axios
+        .get(`${API_SOURCE}/${endpoint}?_page=${page}&_per_page=${perPage}`)
+        .catch(error => {
+          console.log(error);
+          throw new Error('Network response was not ok');
+        });
 
+      return resp.data;
+    }
+  });
+}
+
+function leaseChargeScheduleQuery(endpoint: string, page: number = 1, perPage: number = 5) {
+  return useQuery<ILeaseItems>({
+    queryKey: [endpoint, page, perPage],
+    queryFn: async () => {
+      const resp = await axios
+        .get(`${API_SOURCE}/${endpoint}?_page=${page}&_per_page=${perPage}`)
+        .catch(error => {
+          console.log(error);
+          throw new Error('Network response was not ok');
+        });
+
+      return resp.data;
+    }
+  });
+}
+
+export { leaseQuery, leaseMySpaceQuery, leaseChargeScheduleQuery };
+export type { ILeaseItem, ILeaseItems, ILeaseMySpaceItems, ILeaseChargeScheduleItems, };

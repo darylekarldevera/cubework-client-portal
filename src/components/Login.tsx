ba6a33a71bb8162311885d0ddae2e902094265ab
Dee De Vera <daryle.devera@cubework.com.ph>
2024-09-12 12:24:24 +0800

    Replace percentage value with tailwind css syntax and change color hex into black

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,9 +3,9 @@ import { Input } from './ui/input';
 function Login() {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <div className="border border-solid border-[#000000] rounded-md w-80 h-[27rem] max-w-[100%] max-h-[100%] flex flex-col justify-around p-5">
-        <p className="border-b border-solid border-[#000000] font-bold">Login into your account</p>
-        <form className="flex flex-col justify-between h-[50%] w-full">
+      <div className="border border-solid border border-black rounded-md w-80 h-[27rem] max-w-full max-h-full flex flex-col justify-around p-5">
+        <p className="border-b border-solid border border-black font-bold">Login into your account</p>
+        <form className="flex flex-col justify-between h-1/2 w-full">
           <label>Email</label>
           <Input type="email" placeholder="Enter your email" className="rounded-md" />
           <label>Password</label>
@@ -13,7 +13,7 @@ function Login() {
         </form>
         <div className="flex flex-col">
           <p className="font-bold text-sm">Forgot Password</p>
-          <button className="bg-[#59BA56] w-[80%] h-11 self-center mt-8 rounded-md text-white font-bold cursor-pointer">
+          <button className="bg-[#59BA56] w-4/5 h-11 self-center mt-8 rounded-md text-white font-bold cursor-pointer">
             Confirm
           </button>
         </div>

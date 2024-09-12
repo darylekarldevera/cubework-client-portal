732a6a94db821cd29b3dd95318d06af0c56253fd
Alfred Capacete <alfred.capacete@item.com>
2024-09-13 05:52:20 +0800

    feature/ui-tweaks Move WrappedContent contents to top

--- src/components/WrappedContent.tsx
+++ src/components/WrappedContent.tsx
@@ -8,7 +8,7 @@ interface WrappedContentProps {
 function WrappedContent({ children, className = '' }: WrappedContentProps) {
   return (
     <div className={`h-full flex justify-center w-full x-sm:max-w-[100vw] x-md:max-w-[80vw] x-lg:max-w-[100vw] m-auto ${className}`}>
-      <div className="flex flex-col justify-around h-[100%] flex-grow
+      <div className="flex flex-col justify-start h-full flex-grow
         x-xxs:max-w-[95%] x-md:max-w-[80%]
         px-12
       ">

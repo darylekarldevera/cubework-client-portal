bf1b69f187a3d776011cb1ed7b43e81408520110
darylekarldevera <daryle.devera@cubework.com.ph>
2024-09-16 06:41:01 +0800

    Fix undefined is not iterable

--- src/components/document-page/document-table/TableSearchBar.tsx
+++ src/components/document-page/document-table/TableSearchBar.tsx
@@ -1,19 +1,19 @@
-import {
-  Command,
-  CommandDialog,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-  CommandSeparator,
-  CommandShortcut,
-} from '@/components/ui/command';
+import React from 'react';
+import { CommandItem, CommandList } from 'cmdk';
+import { Command, CommandInput } from '@/components/ui/command';
 
-function TableSearchBar() {
+interface TableSearchBarProps {
+  search: string;
+  setSearch: React.Dispatch<React.SetStateAction<string>>;
+}
+
+function TableSearchBar({ search, setSearch }: TableSearchBarProps) {
   return (
-    <Command  >
-      <CommandInput placeholder="Type a command or search..." />
+    <Command value={search} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearch(e.target.value)}>
+      <CommandInput placeholder="Type a command or search..."/>
+      <CommandList>
+        <CommandItem />
+      </CommandList>
     </Command>
   );
 }

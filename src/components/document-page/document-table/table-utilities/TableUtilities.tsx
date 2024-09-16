bee15f10a38851bf55787e962f9c97e0294f962e
darylekarldevera <daryle.devera@cubework.com.ph>
2024-09-16 11:37:43 +0800

    Refactor TableUtilities component to use generic type for data and originalData props, and add utilityInstance prop

--- src/components/document-page/document-table/table-utilities/TableUtilities.tsx
+++ src/components/document-page/document-table/table-utilities/TableUtilities.tsx
@@ -1,36 +1,37 @@
 import React, { useState } from 'react'
-import { IDocument } from '@/types/invoiceDocuments';
 
 import TableUtilitiesControllers from './TableUtilitiesControllers'
 import TableUtilitySorterAndFilter from './TableUtilitySorterAndFilter'
+import DocumentTableUtility from '@/lib/documentDataSorterAndFilter';
 
 interface TableUtilitiesProps<T> {
-  data: IDocument[];
-  originalData: IDocument[];
-  setData: React.Dispatch<React.SetStateAction<IDocument[]>>;
+  data: T[];
+  originalData: T[];
+  setData: React.Dispatch<React.SetStateAction<T[]>>;
   filterCb: (item: T[], searchData: string) => T[];
+  utilityInstance: DocumentTableUtility<T>;
 }
 
-function TableUtilities<T>({ data, originalData, setData, filterCb }: TableUtilitiesProps<T>) {
+function TableUtilities<T>({ data, originalData, utilityInstance, setData, filterCb }: TableUtilitiesProps<T>) {
   const [openUtility, setOpenUtility] = useState<string>('');
   
   return (
     <div className="pb-3">
       <TableUtilitiesControllers
         openUtility={openUtility}
-        originalData={originalData} 
-        setData={setData} 
+        originalData={originalData}
+        setData={setData}
         filterCb={filterCb}
         setOpenUtility={setOpenUtility}
       />
       <TableUtilitySorterAndFilter
         data={data}
-        originalData={originalData}
-        openUtility={openUtility} 
-        setDocumentsData={setData} 
+        openUtility={openUtility}
+        setData={setData}
+        utilityInstance={utilityInstance}
       />
     </div>
-  )
+  );
 }
 
 export default TableUtilities

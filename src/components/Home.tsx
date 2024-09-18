dbc6dd71d4487f67072ac5cbd32a49f0a6f6e394
darylekarldevera <daryle.devera@cubework.com.ph>
2024-09-18 13:52:05 +0800

    Refactor Home component to integrate with mock API data and implement searching, sorting and filtering functionality

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from 'react';
-import { HOME_ACTIVITY_TABLE_MOCK } from '@/__mocks__/homeActivityTableMock';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { HOME_ACTIVITY_TABLE_COLUMNS } from '@/constants/homeActivityTableColumns';
 import IHomeActivityTable from '@/types/homeActivityTable';
 
@@ -7,33 +6,61 @@ import DataTable from './data-table/DataTable';
 import { Heading1 } from './ui/headings';
 import PaymentBalanceCard from './PaymentBalanceCard';
 import CWCard from './CWCard';
+import { HomeQuery } from '@/queries/HomeQuery';
+import HomeTableUtility from '@/lib/homeDataSorterAndFilter';
+import TableUtilities from './document-page/document-table/table-utilities/TableUtilities';
 
-// placeholder for fetching data
-const getData = async (): Promise<IHomeActivityTable[]> => {
-  const data = [...Array(50).fill(HOME_ACTIVITY_TABLE_MOCK)].flat();
-  const newData = data.map((item, index) => {
-    return {
-      ...item,
-      id: index + 1,
-      amount: Math.floor(Math.random() * 1000),
-    };
-  });
-
-  return newData;
-};
 
 function Home() {
-  const [activityTableData, setActivityTableData] = useState<IHomeActivityTable[]>([]);
+  const [originalData, setOriginalData] = useState<IHomeActivityTable[]>([]);
+  const [documentsData, setDocumentsData] = useState<IHomeActivityTable[]>([]);
+  const homeData = HomeQuery('invoice_documents');
+
+  const filterCb = useCallback((items: IHomeActivityTable[], searchData: string): IHomeActivityTable[] => {
+    return items.filter((item) => {
+      // Matches any non-alphanumeric character and underscores
+      const regex = /[\W_]+/g;
+      const search = searchData.toLowerCase()?.replace(regex, '');
+      const itemType = item?.type?.toLowerCase()?.replace(regex, '');
+      const itemMethod = item?.method?.toLowerCase()?.replace(regex, '');
+      return itemType.includes(search.toLowerCase()) || itemMethod.includes(search.toLowerCase());
+    });
+  }, []);
+
+  // Memoize the utility instance only when the homeData data changes
+  const utility = useMemo(() => {
+    if (homeData.isSuccess && homeData.data) {
+      // Use spread operator to avoid mutating original data
+      const utilityInstance = new HomeTableUtility<IHomeActivityTable>([...homeData.data]);
+      const sortedData = utilityInstance.sortData();
+      setDocumentsData(sortedData);
+      setOriginalData(sortedData);
+      return utilityInstance;
+    }
+    return new HomeTableUtility<IHomeActivityTable>([]);
+  }, [homeData.data, homeData.isSuccess]);
 
   useEffect(() => {
-    getData()
-      .then((data) => {
-        setActivityTableData(data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    if (homeData.isSuccess && homeData.data) {
+      // Sort the data and update the state
+      const sortedData = utility.sortData();
+      setDocumentsData(sortedData);
+      setOriginalData(sortedData);
+    }
+
+    if (homeData.isError) {
+      setDocumentsData([]);
+      setOriginalData([]);
+    }
   }, []);
+
+  if (homeData.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (homeData.isError) {
+    return <div>Error...</div>;
+  }
 
   return (
     <div className="pb-[3%]">
@@ -45,8 +72,42 @@ function Home() {
         <p className="text-xs text-cw-green mb-2 w-[15%] text-center">Activity</p>
       </div>
 
+      <TableUtilities
+        data={documentsData}
+        originalData={originalData}
+        setData={setDocumentsData}
+        utilityInstance={utility}
+        sortOptions={[
+          {
+            name: 'Date',
+            sortType: {
+              asc: 'ASC',
+              desc: 'DESC',
+            },
+          },
+        ]} 
+        filterOptions={[
+          {
+            name: 'All',
+            filterType: 'all',
+          },
+          {
+            name: 'Pre - Authorized Payment',
+            filterType: 'preAuthorizedPayment',
+          },
+          {
+            name: 'Rent - Warehouse',
+            filterType: 'rentWarehouse',
+          },
+          {
+            name: 'Forklift Rental',
+            filterType: 'forkliftRental',
+          },
+        ]}
+        filterCb={filterCb}
+      />
       <CWCard>
-        <DataTable pageSize={5} data={activityTableData} columns={HOME_ACTIVITY_TABLE_COLUMNS} cwStyle={true} />
+        <DataTable pageSize={5} data={documentsData} columns={HOME_ACTIVITY_TABLE_COLUMNS} cwStyle={true} />
       </CWCard>
     </div>
   );

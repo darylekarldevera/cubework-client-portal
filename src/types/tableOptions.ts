f42d67839b2a6d868643632addabe41a533664c3
darylekarldevera <daryle.devera@cubework.com.ph>
2024-09-24 05:08:09 +0800

    Add tableOptions, tableProps, and tableUtility types

---
+++ src/types/tableOptions.ts
@@ -0,0 +1,13 @@
+export interface ISortOption {
+  name: string;
+  sortType: {
+    [key: string]: string;
+    asc: string;
+    desc: string;
+  };
+}
+
+export interface IFilterOption {
+  name: string;
+  filterType: string;
+}

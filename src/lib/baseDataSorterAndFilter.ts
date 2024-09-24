1029dccc76ae62c9328ad65b0c5b4dc8dccd7f98
darylekarldevera <daryle.devera@cubework.com.ph>
2024-09-24 12:04:09 +0800

    Refactor document and home table utilities to extend baseDataSorterAndFilter for shared functions and logic

---
+++ src/lib/baseDataSorterAndFilter.ts
@@ -0,0 +1,76 @@
+import { ITableUtility } from '@/types/tableUtility';
+import { ICheckboxProps } from '@/types/tableProps';
+
+class BaseDataSorterAndFilter<DataType> implements ITableUtility<DataType, ICheckboxProps> {
+  protected data: DataType[];
+  protected checkbox?: ICheckboxProps;
+
+  constructor(data: DataType[], checkbox?: ICheckboxProps) {
+    this.data = data;
+    this.checkbox = checkbox;
+  }
+
+  setCheckbox = (checkbox: ICheckboxProps) => {
+    this.checkbox = checkbox;
+  };
+
+  sortAndFilter = (): DataType[] => {
+    const sortedData = this.sortData();
+    const sortedAndFilteredData = this.filterData(sortedData);
+    return sortedAndFilteredData;
+  };
+
+  sortData = () => {
+    const toUpdateData = [...this.data];
+    const sortedDataByDate = this.sortDataByDate(toUpdateData);
+    return sortedDataByDate;
+  };
+
+  filterData(data: DataType[]): DataType[] {
+    const toUpdateData = data?.length ? [...data] : [];
+    const filteredDataByDate = this.filterByDate(toUpdateData);
+    return filteredDataByDate;
+  }
+
+  sortDataByDate(data: DataType[]): DataType[] {
+    const toUpdateData = [...data];
+    const sortName = this?.checkbox?.sort?.name?.toLowerCase();
+    const sortType = this?.checkbox?.sort?.sortType?.toLowerCase();
+
+    if (sortName === 'date' && sortType === 'ascending') {
+      return toUpdateData.sort((a, b) => {
+        return (a as any).date > (b as any).date ? 1 : -1;
+      });
+    }
+
+    return toUpdateData.sort((a, b) => {
+      return (a as any).date < (b as any).date ? 1 : -1;
+    });
+  }
+
+  filterByDate = (data: DataType[]) => {
+    let toUpdateData = data?.length ? [...data] : [];
+    const startDate = this?.checkbox?.filter?.pickDate?.startDate;
+    const endDate = this?.checkbox?.filter?.pickDate?.endDate;
+
+    if (startDate && endDate) {
+      toUpdateData = toUpdateData.filter((item) => {
+        const itemDate = new Date((item as any).date);
+
+        if (startDate && endDate) {
+          return itemDate >= startDate && itemDate <= endDate;
+        }
+
+        if (startDate) {
+          return itemDate >= startDate;
+        }
+
+        return false;
+      });
+    }
+
+    return toUpdateData;
+  };
+}
+
+export default BaseDataSorterAndFilter;
--- src/lib/documentDataSorterAndFilter.ts
+++ src/lib/documentDataSorterAndFilter.ts
@@ -1,21 +1,16 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { IDocument } from "@/types/invoiceDocuments";
-import { ITableUtility } from "@/types/tableUtility";
 import { ICheckboxProps } from "@/types/tableProps";
+import BaseDataSorterAndFilter from "./baseDataSorterAndFilter";
 
-class DocumentTableUtility implements ITableUtility<IDocument, ICheckboxProps> {
-  private data: IDocument[];
-  private checkbox?: ICheckboxProps;
+class DocumentTableUtility extends BaseDataSorterAndFilter<IDocument> {
+  protected data: IDocument[];
+  protected checkbox?: ICheckboxProps;
 
   constructor(data: IDocument[], checkbox?: ICheckboxProps){
+    super(data, checkbox);
     this.data = data;
     this.checkbox = checkbox;
-  }
-
-  sortAndFilter = (): IDocument[] => {
-    let toUpdateData = [...this.data];
-    toUpdateData = this.sortData();
-    toUpdateData = this.filterData(toUpdateData);
-    return toUpdateData;
   }
 
   sortData = () => {
@@ -35,49 +30,24 @@ class DocumentTableUtility implements ITableUtility<IDocument, ICheckboxProps> {
       });
     }
 
-    if (sortName === "date" && sortType === "ascending") {
-      return toUpdateData.sort((a, b) => {
-        return a.date > b.date ? 1 : -1;
-      });
-    }
-
-    return toUpdateData.sort((a, b) => {
-      return a.date < b.date ? 1 : -1;
-    });
+    return this.sortDataByDate(toUpdateData);
   };
 
   filterData = (data: IDocument[]) => {
-    let toUpdateData = [...data];
+    const toUpdateData = [...data];
+    const filteredDataByDate = this.filterByDate(toUpdateData);
+
     // Filter by name and type of document (invoice, statement, etc.)
+    // Save these variables for future use
+    // @ts-expect-error
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const filterName = this?.checkbox?.filter?.name?.toLowerCase();
+
+    // @ts-expect-error
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const filterType = this?.checkbox?.filter?.filterType?.toLowerCase();
 
-    // Filter by date
-    const startDate = this?.checkbox?.filter?.pickDate?.startDate;
-    const endDate = this?.checkbox?.filter?.pickDate?.endDate;
-
-    // filter by date range before filtering by name and type
-    if (startDate && endDate) {
-      toUpdateData = toUpdateData.filter((item) => {
-        const itemDate = new Date(item.date);
-
-        if (startDate && endDate) {
-          return itemDate >= startDate && itemDate <= endDate;
-        }
-
-        if (startDate) {
-          return itemDate >= startDate;
-        }
-
-        return false;
-      });
-    }
-
-    return toUpdateData;
-  };
-
-  setCheckbox = (checkbox: ICheckboxProps) => {
-    this.checkbox = checkbox;
+    return filteredDataByDate;
   };
 }
 

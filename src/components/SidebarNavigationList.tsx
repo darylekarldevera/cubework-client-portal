cc4aeb451816c64f8173ba6fc84437a45e5b7151
darylekarldevera <daryle.devera@cubework.com.ph>
2024-09-26 12:28:13 +0800

    Refactor SidebarNavigationList component to improve tab highlighting logic

--- src/components/SidebarNavigationList.tsx
+++ src/components/SidebarNavigationList.tsx
@@ -9,9 +9,11 @@ interface ISidebarNavigationList {
 }
 
 function SidebarNavigationList({ className }: ISidebarNavigationList) {
-  const { setOpenSidebar } = useContext(SidebarContext);
-
   const { pathname } = useLocation();
+  const { setOpenSidebar } = useContext(SidebarContext);
+  
+  const isCurrentTab = (pathname: string, tab: string) => getCurrentTab(pathname) === tab;
+
   return (
     <div className={`flex-grow-3 pt-3 ${className}`} onClick={() => setOpenSidebar((prev: boolean) => !prev)}>
       {NAVIGATION_ITEMS.map((item, index) => (
@@ -19,23 +21,23 @@ function SidebarNavigationList({ className }: ISidebarNavigationList) {
           key={index}
           to={item.path}
           className={`
-              pl-7 p-3 text-center cursor-pointer transition-all duration-100 hover:text-white flex flex-row items-center group hover:bg-cw-green
-              ${getCurrentTab(pathname) === item.name ? 'text-white bg-cw-green' : 'text-black'}  
-            `}
+            pl-7 p-3 text-center cursor-pointer transition-all duration-100 hover:text-white flex flex-row items-center group hover:bg-cw-green
+            ${isCurrentTab(pathname, item.name) ? 'text-white bg-cw-green' : 'text-black'}  
+          `}
         >
           <img
             alt={item.alt}
             src={item.img.blackIcon}
             className={`group-hover:hidden h-4
-                ${getCurrentTab(pathname) === item.name ? 'hidden group-hover:block' : 'block'}  
+                ${isCurrentTab(pathname, item.name) ? 'hidden group-hover:block' : 'block'}  
               `}
           />
           <img
             alt={item.alt}
             src={item.img.whiteIcon}
             className={`group-hover:block h-4
-                ${getCurrentTab(pathname) === item.name ? 'block group-hover:block 4' : 'hidden'}  
-              `}
+              ${isCurrentTab(pathname, item.name) ? 'block group-hover:block 4' : 'hidden'}  
+            `}
           />
           <p className="w-full text-start pl-5 font-bold text-cb-text leading-relaxed">{item.name}</p>
         </Link>

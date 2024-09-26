9e5a73903cb2fcbf35ce2ba8eb8a442e394a0957
Alfred Capacete <alfred.capacete@item.com>
2024-09-26 08:11:29 +0800

    CUBE-46 Add modal lease query error

--- src/queries/LeaseQuery.ts
+++ src/queries/LeaseQuery.ts
@@ -2,6 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 import { API_SOURCE } from "@/constants/apiSource";
+import { useContext } from "react";
+import { ErrorModalContext } from "@/contexts/ErrorModalContext";
 
 interface ILeaseItem {
   id: number,
@@ -41,12 +43,15 @@ interface ILeaseChargeScheduleItems {
 }
 
 function leaseQuery<T extends ILeaseItems | ILeaseMySpaceItems | ILeaseChargeScheduleItems>(endpoint: string, page: number = 1, perPage: number = 5) {
+  const { setShowError } = useContext(ErrorModalContext);
+
   return useQuery<T>({
     queryKey: [endpoint, page, perPage],
     queryFn: async () => {
       const resp = await axios
         .get(`${API_SOURCE}/${endpoint}?_page=${page}&_per_page=${perPage}`)
         .catch(error => {
+          setShowError(true);
           console.log(error);
           throw new Error('Network response was not ok');
         });
